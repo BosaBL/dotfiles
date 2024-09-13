affeda86ff0fed7d3873cf977654a77fa1d4ea38
Christopher Sepúlveda <Christopher>
2024-09-13 19:34:39 -0300

    first commit

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,9 @@
+alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME/'
+
+# Default replacement aliases
+alias cat='bat'
+alias ls='eza --icons=always --colour=always'
+
+# Nvim aliases
+alias vim="nvim"
+alias vi="nvim"
