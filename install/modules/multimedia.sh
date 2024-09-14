70777433a8eb0c9f529b68ce811ef0c79a18bec5
Christopher <christopher172003@outlook.cl>
2024-09-14 19:39:57 -0300

    feat(install-scripts): install scripts progress
    - Still missing dotfiles auto install.

---
+++ install/modules/multimedia.sh
@@ -0,0 +1,5 @@
+
+sudo dnf -y swap 'ffmpeg-free' 'ffmpeg' --allowerasing
+sudo dnf -y group install Multimedia
+sudo dnf -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf -y update @sound-and-video
