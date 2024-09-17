d2ee067ec32376b4b884131fc1087e1f5cdfa859
Christopher <christopher172003@outlook.cl>
2024-09-17 20:54:50 -0300

    feat: added wrapping by default or .tex and .bib files

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,20 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Vimtex configuration
-vim.g["vimtex_compiler_method"] = "latexmk"
-vim.g["vimtex_view_method"] = "zathura"
-vim.g["vimtex_quickfix_open_on_warning"] = 0
-vim.g["vimtex_compiler_latexmk"] = {
-  executable = "latexmk",
-  options = {
-    "-verbose",
-    "-file-line-error",
-    "-synctex=1",
-    "-interaction=nonstopmode",
-    "--shell-escape",
-  },
-}
-
 -- Leader remaps
 vim.g.maplocalleader = " "
+
+-- Disable comment continuation
+vim.opt_local.formatoptions:remove({ "r", "o" })
--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,7 +4,30 @@ return {
   config = function()
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+
+    -- Vimtex configuration
+    vim.g["vimtex_compiler_method"] = "latexmk"
+    vim.g["vimtex_view_method"] = "zathura"
+    vim.g["vimtex_quickfix_open_on_warning"] = 0
+    vim.g["vimtex_compiler_latexmk"] = {
+      executable = "latexmk",
+      options = {
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+        "--shell-escape",
+      },
+    }
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "tex", "bib" },
+      callback = function(event)
+        vim.opt_local.wrap = true
+      end,
+    })
   end,
+
   keys = {
     { "<localLeader>l", "", desc = "+vimtext" },
   },
