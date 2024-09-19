21dd3a48bec17c9f261bea98e7cc7240a3e5dc47
Christopher <christopher172003@outlook.cl>
2024-09-19 17:54:24 -0300

    feat: created assets folder

--- .dotfiles-install/modules/sddm.sh
+++ .dotfiles-install/modules/sddm.sh
@@ -1,7 +1,7 @@
 installPackage "sddm"
 
-sudo cp -r sddm-astronaut-theme /usr/share/sddm/themes
-sudo cp ./sddm.conf /etc/
+sudo cp -r "$SCRIPT_DIR"/assets/sddm-astronaut-theme /usr/share/sddm/themes
+sudo cp "$SCRIPT_DIR"/assets/sddm.conf /etc/
 
 sudo systemctl set-default graphical.target
 sudo systemctl enable sddm.service
