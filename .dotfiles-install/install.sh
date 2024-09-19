02e9e7c22febd75448b754d26fff27a344f1745c
Christopher <christopher172003@outlook.cl>
2024-09-19 17:51:45 -0300

    fix: fixed install scripts

--- .dotfiles-install/install.sh
+++ .dotfiles-install/install.sh
@@ -2,8 +2,10 @@
 
 LOG_FILE="install-$(date +%d-%H%M%S).log"
 TMP_DIR="$HOME/Downloads/tmp"
-chmod +x ./helpers.sh
-chmod +x ./modules/*
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+chmod +x "$SCRIPT_DIR"/helpers.sh
+chmod +x "$SCRIPT_DIR"/modules/*
 
 nvidiaFlag=""
 yes_or_no "Do you want to install Nvidia drivers?" nvidiaFlag
@@ -11,31 +13,31 @@ yes_or_no "Do you want to install Nvidia drivers?" nvidiaFlag
 echo "Appending DNF parallel downloads"
 grep -qxF 'max_parallel_downloads=20' /etc/dnf/dnf.conf || echo 'max_parallel_downloads=20' | sudo tee -a /etc/dnf/dnf.conf >/dev/null
 
-source ./helpers.sh
-source ./modules/rpmfusion-copr.sh
+source "$SCRIPT_DIR"/helpers.sh
+source "$SCRIPT_DIR"/modules/rpmfusion-copr.sh
 
 if [ "$nvidiaFlag" == "Y" ]; then
-  source ./modules/nvidia.sh
+  source "$SCRIPT_DIR"/modules/nvidia.sh
 fi
 
-source ./modules/packages.sh
-source ./modules/kdeconnect.sh
-source ./modules/sddm.sh
-source ./modules/multimedia.sh
-source ./modules/zsh.sh
+source "$SCRIPT_DIR"/modules/packages.sh
+source "$SCRIPT_DIR"/modules/kdeconnect.sh
+source "$SCRIPT_DIR"/modules/sddm.sh
+source "$SCRIPT_DIR"/modules/multimedia.sh
+source "$SCRIPT_DIR"/modules/zsh.sh
 
 mkdir -p "$TMP_DIR"
 cd "$TMP_DIR" || exit
 
 # These need a new shell instance
 echo "Ngw-look"
-./modules/ngwlook.sh
+"$SCRIPT_DIR"/modules/ngwlook.sh
 echo "Installing Themes"
-./modules/themes.sh
+"$SCRIPT_DIR"/modules/themes.sh
 echo "Installing Fonts"
-./modules/nerdfonts.sh
+"$SCRIPT_DIR"/modules/nerdfonts.sh
 echo "Installing Dotfiles"
-./modules/installdotfiles.sh
+"$SCRIPT_DIR"/modules/installdotfiles.sh
 echo "Dotfiles Installed"
 
 cd "$HOME" || exit
