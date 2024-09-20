235212f8db8d7209b9c29f196a39a79c9fdbc7c1
Christopher <christopher172003@outlook.cl>
2024-09-20 18:39:23 -0300

    feat: sync SDDM wallpaper to desk wallpaper

--- .dotfiles-install/modules/sddm.sh
+++ .dotfiles-install/modules/sddm.sh
@@ -3,5 +3,7 @@ installPackage "sddm"
 sudo cp -r "$SCRIPT_DIR"/assets/sddm-astronaut-theme /usr/share/sddm/themes
 sudo cp "$SCRIPT_DIR"/assets/sddm.conf /etc/
 
+sudo chmod -R o+wrx /usr/share/sddm/themes/sddm-astronaut-theme
+
 sudo systemctl set-default graphical.target
 sudo systemctl enable sddm.service
