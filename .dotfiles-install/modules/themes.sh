f8e4f7e52823af12783d3da882439c6cdf953aa7
Christopher <christopher172003@outlook.cl>
2024-09-22 16:58:45 -0300

    feat: added bibata to SDDM and fixed user shell detection

--- .dotfiles-install/modules/themes.sh
+++ .dotfiles-install/modules/themes.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+if [ ! -d "$HOME"/.local/share/icons ]; then
+  mkdir -p "$HOME"/.local/share/icons
+fi
+
+echo "Installing cursors"
 tar -xzvf "$SCRIPT_DIR"/assets/cursors.tar.gz -C "$HOME/.local/share/icons"
+sudo cp -r "$HOME"/.local/share/icons/Bibata/ /usr/share/icons/
 
 git clone https://github.com/vinceliuice/Orchis-kde
 git clone https://github.com/vinceliuice/Orchis-theme
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 
+echo "Installing Orchis-kde"
 cd Orchis-kde || exit
 ./install.sh
 
+echo "Installing Orchis-theme"
 cd ..
 cd Orchis-theme || exit
 ./install.sh
 
+echo "Installing Tela-circle-icon-theme"
 cd ..
 cd Tela-circle-icon-theme || exit
 ./install.sh blue
