fd530b8ebfee6481b3fa3ef5256b8da12a806caa
Christopher <christopher172003@outlook.cl>
2024-10-10 22:11:34 -0300

    feat(nvim): add latex snips, improve latex snips

--- .config/nvim/lua/helpers/funcs.lua
+++ .config/nvim/lua/helpers/funcs.lua
@@ -1,26 +1,35 @@
-local M = {}
-
 local ls = require("luasnip")
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep
+local M = {}
+s = ls.snippet
+sn = ls.snippet_node
+t = ls.text_node
+i = ls.insert_node
+f = ls.function_node
+c = ls.choice_node
+d = ls.dynamic_node
+r = ls.restore_node
+l = require("luasnip.extras").lambda
+rep = require("luasnip.extras").rep
+p = require("luasnip.extras").partial
+m = require("luasnip.extras").match
+n = require("luasnip.extras").nonempty
+dl = require("luasnip.extras").dynamic_lambda
+fmt = require("luasnip.extras.fmt").fmt
+fmta = require("luasnip.extras.fmt").fmta
+types = require("luasnip.util.types")
+conds = require("luasnip.extras.conditions")
+conds_expand = require("luasnip.extras.conditions.expand")
 
 function M.get_ISO_8601_date()
-  return os.date("%Y-%m-%d")
+	return os.date("%Y-%m-%d")
 end
 
 function M.get_visual(args, parent)
-  if #parent.snippet.env.LS_SELECT_RAW > 0 then
-    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
-  else
-    return sn(nil, i(1, ""))
-  end
+	if #parent.snippet.env.LS_SELECT_RAW > 0 then
+		return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+	else
+		return sn(nil, i(1, ""))
+	end
 end
 
 return M
--- .config/nvim/luasnip/tex/enviroments.lua
+++ .config/nvim/luasnip/tex/enviroments.lua
@@ -1,265 +1,256 @@
-local ls = require("luasnip")
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep
 local helpers = require("helpers.funcs")
-local get_visual = helpers.get_visual
 
 -- Math context detection
 local tex = {}
 tex.in_mathzone = function()
-  return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 end
 tex.in_text = function()
-  return not tex.in_mathzone()
+	return not tex.in_mathzone()
 end
 
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
+local get_visual = helpers.get_visual
+
 -- Return snippet tables
 return {
-  -- GENERIC ENVIRONMENT
-  s(
-    { trig = "env", snippetType = "autosnippet" },
-    fmta(
-      [[
+	-- GENERIC ENVIRONMENT
+	s(
+		{ trig = "env", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{<>}
             <>
         \end{<>}
       ]],
-      {
-        i(1),
-        d(2, get_visual),
-        rep(1),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- ENVIRONMENT WITH ONE EXTRA ARGUMENT
-  s(
-    { trig = "n2", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				d(2, get_visual),
+				rep(1),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- ENVIRONMENT WITH ONE EXTRA ARGUMENT
+	s(
+		{ trig = "n2", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{<>}{<>}
             <>
         \end{<>}
       ]],
-      {
-        i(1),
-        i(2),
-        d(3, get_visual),
-        rep(1),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- ENVIRONMENT WITH TWO EXTRA ARGUMENTS
-  s(
-    { trig = "n3", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				i(2),
+				d(3, get_visual),
+				rep(1),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- ENVIRONMENT WITH TWO EXTRA ARGUMENTS
+	s(
+		{ trig = "n3", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{<>}{<>}{<>}
             <>
         \end{<>}
       ]],
-      {
-        i(1),
-        i(2),
-        i(3),
-        d(4, get_visual),
-        rep(1),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- CUSTOM THEOREM ENVIROMENT
-  s(
-    { trig = "theo", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				i(2),
+				i(3),
+				d(4, get_visual),
+				rep(1),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- CUSTOM THEOREM ENVIROMENT
+	s(
+		{ trig = "theo", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{theorem}{<>}
             <>
         \end{theorem}
       ]],
-      {
-        i(1),
-        d(2, get_visual),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- CUSTOM COROLARY ENVIROMENT
-  s(
-    { trig = "cor", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				d(2, get_visual),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- CUSTOM COROLARY ENVIROMENT
+	s(
+		{ trig = "cor", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{corollary}[<>]
             <>
         \end{theorem}
       ]],
-      {
-        i(1),
-        d(2, get_visual),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- CUSTOM DEFINITION ENVIROMENT
-  s(
-    { trig = "def", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				d(2, get_visual),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- CUSTOM DEFINITION ENVIROMENT
+	s(
+		{ trig = "def", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{definition}[<>]
             <>
         \end{definition}
       ]],
-      {
-        i(1),
-        d(2, get_visual),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- CUSTOM PROBLEM ENVIROMENT
-  s(
-    { trig = "prob", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				d(2, get_visual),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- CUSTOM PROBLEM ENVIROMENT
+	s(
+		{ trig = "prob", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{prob}\label{prob:<>}
             <>
         \end{prob}
       ]],
-      {
-        i(1),
-        i(2),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- CUSTOM SOLUTON ENVIROMENT
-  s(
-    { trig = "sol", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				i(2),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- CUSTOM SOLUTON ENVIROMENT
+	s(
+		{ trig = "sol", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{sol}[\cref{sol:<>}]
             <>
         \end{sol}
       ]],
-      {
-        i(1),
-        i(2),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- EQUATION
-  s(
-    { trig = "eq", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+				i(2),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- EQUATION
+	s(
+		{ trig = "eq", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{equation*}
             <>
         \end{equation*}
       ]],
-      {
-        i(1),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- SPLIT EQUATION
-  s(
-    { trig = "seq", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- SPLIT EQUATION
+	s(
+		{ trig = "seq", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{equation*}
             \begin{split}
                 <>
             \end{split}
         \end{equation*}
       ]],
-      {
-        d(1, get_visual),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- ALIGN
-  s(
-    { trig = "all", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				d(1, get_visual),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- ALIGN
+	s(
+		{ trig = "all", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{align*}
             <>
         \end{align*}
       ]],
-      {
-        i(1),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- ITEMIZE
-  s(
-    { trig = "itt", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(1),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- ITEMIZE
+	s(
+		{ trig = "itt", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{itemize}
 
             \item <>
 
         \end{itemize}
       ]],
-      {
-        i(0),
-      }
-    ),
-    { condition = line_begin }
-  ),
-  -- ENUMERATE
-  s(
-    { trig = "enum", snippetType = "autosnippet" },
-    fmta(
-      [[
+			{
+				i(0),
+			}
+		),
+		{ condition = line_begin }
+	),
+	-- ENUMERATE
+	s(
+		{ trig = "enum", snippetType = "autosnippet" },
+		fmta(
+			[[
         \begin{enumerate}
 
             \item <>
 
         \end{enumerate}
       ]],
-      {
-        i(0),
-      }
-    )
-  ),
-  -- INLINE MATH
-  s(
-    { trig = "([^%l])mm", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
-    fmta("<>$<>$", {
-      f(function(_, snip)
-        return snip.captures[1]
-      end),
-      d(1, get_visual),
-    })
-  ),
-  -- INLINE MATH ON NEW LINE
-  s(
-    { trig = "^mm", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
-    fmta("$<>$", {
-      i(1),
-    })
-  ),
-  -- FIGURE
-  s(
-    { trig = "fig" },
-    fmta(
-      [[
+			{
+				i(0),
+			}
+		)
+	),
+	-- INLINE MATH
+	s(
+		{ trig = "([^%l])mm", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
+		fmta("<>$<>$", {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			d(1, get_visual),
+		})
+	),
+	-- INLINE MATH ON NEW LINE
+	s(
+		{ trig = "^mm", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
+		fmta("$<>$", {
+			i(1),
+		})
+	),
+	-- FIGURE
+	s(
+		{ trig = "fig" },
+		fmta(
+			[[
         \begin{figure}[htb!]
           \centering
           \includegraphics[width=<>\linewidth]{<>}
@@ -267,13 +258,13 @@ return {
           \label{fig:<>}
         \end{figure}
         ]],
-      {
-        i(1),
-        i(2),
-        i(3),
-        i(4),
-      }
-    ),
-    { condition = line_begin }
-  ),
+			{
+				i(1),
+				i(2),
+				i(3),
+				i(4),
+			}
+		),
+		{ condition = line_begin }
+	),
 }
