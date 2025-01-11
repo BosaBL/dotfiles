657c98ce5358b72b8f2659b57158927ff236d482
Christopher <christopher172003@outlook.cl>
2025-01-11 19:26:01 -0300

    fix(blink-cmp,luasnip): fix snippet completion

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -12,7 +12,7 @@ return {
 				"rafamadriz/friendly-snippets",
 				config = function(_, opts)
 					require("luasnip.loaders.from_lua").lazy_load({ paths = { "./luasnip/" } })
-					require("luasnip.loaders.from_vscode").lazy_load({ exclude = "tex" })
+					require("luasnip.loaders.from_vscode").lazy_load({ exclude = { "tex" } })
 				end,
 			},
 		},
@@ -28,29 +28,9 @@ return {
 		"saghen/blink.cmp",
 		opts = {
 			keymap = {
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-e>"] = { "hide", "fallback" },
-
-				["<Tab>"] = {
-					function(cmp)
-						if cmp.snippet_active() then
-							return cmp.accept()
-						else
-							return cmp.select_and_accept()
-						end
-					end,
-					"snippet_forward",
-					"fallback",
-				},
-				["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-				["<Up>"] = { "select_prev", "fallback" },
-				["<Down>"] = { "select_next", "fallback" },
-				["<C-p>"] = { "select_prev", "fallback" },
-				["<C-n>"] = { "select_next", "fallback" },
-
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+				preset = "enter",
+				["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "select_next", "snippet_backward", "fallback" },
 			},
 			snippets = {
 				expand = function(snippet)
@@ -66,17 +46,12 @@ return {
 					require("luasnip").jump(direction)
 				end,
 			},
+			completion = {
+				list = { selection = { preselect = false } },
+			},
 			sources = {
-				default = { "lsp", "path", "luasnip", "buffer" },
+				default = { "lsp", "path", "snippets", "buffer" },
 			},
 		},
-	},
-	{
-		"saghen/blink.cmp",
-		opts = function(_, opts)
-			opts.sources.default = vim.tbl_filter(function(p)
-				return p ~= "snippets"
-			end, opts.sources.default)
-		end,
 	},
 }
