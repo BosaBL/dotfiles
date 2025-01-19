4e17a9d010375599714865321b5eea898cd98ce8
Christopher <christopher172003@outlook.cl>
2025-01-19 17:06:34 -0300

    feat(multimedia): add new multimedia packages

--- .dotfiles-install/modules/multimedia.sh
+++ .dotfiles-install/modules/multimedia.sh
@@ -6,3 +6,7 @@ sudo dnf -y swap 'ffmpeg-free' 'ffmpeg' --allowerasing
 sudo dnf -y group install Multimedia
 sudo dnf -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf -y update @sound-and-video
+
+sudo dnf -y install ffmpeg-libs libva libva-utils
+sudo dnf -y swap libva-intel-media-driver intel-media-driver --allowerasing
+sudo dnf -y install libva-intel-driver
