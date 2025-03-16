76ccacfa0ac1ac2fcb157cc3de3af203aaa5d43a
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-03-16 18:50:46 -0300

    fix: added missing nvidia packages and support for suspend/hibernate

--- .dotfiles-install/modules/nvidia.sh
+++ .dotfiles-install/modules/nvidia.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 packages=(
-    akmod-nvidia
-    xorg-x11-drv-nvidia-cuda
-    xorg-x11-drv-nvidia-cuda-libs
+  akmod-nvidia
+  xorg-x11-drv-nvidia-cuda
+  xorg-x11-drv-nvidia-cuda-libs
+  nvidia-vaapi-driver
+  libva-utils
+  vdpauinfo
+  xorg-x11-drv-nvidia-power
 )
 
 installPackages "${packages[@]}"
+sudo systemctl enable nvidia-{suspend,resume,hibernate}
