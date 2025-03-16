22b2c7d7bb43c07d4303bc3fe2b522858662956d
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-03-16 18:50:44 -0300

    feat(install-scripts): install scripts progress
    - Still missing dotfiles auto install.

---
+++ install/modules/nerdfonts.sh
@@ -0,0 +1,13 @@
+mkdir -p $HOME/Downloads/tmp
+cd $HOME/Downloads/tmp
+
+DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
+curl -OL "$DOWNLOAD_URL"
+# Check if the JetBrainsMono folder exists and delete it if it does
+if [ -d ~/.local/share/fonts/JetBrainsMonoNerd ]; then
+    rm -rf ~/.local/share/fonts/JetBrainsMonoNerd
+fi
+mkdir -p ~/.local/share/fonts/JetBrainsMonoNerd
+tar -xJkf JetBrainsMono.tar.xz -C ~/.local/share/fonts/JetBrainsMonoNerd 2>&1 | tee -a "$LOG"
+
+fc-cache -v
