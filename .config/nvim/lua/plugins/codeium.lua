f83a45c34d4e4bb363db6298946cb3f57972c851
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-03-16 18:50:46 -0300

    fix: codeium

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,5 +1,32 @@
 return {
-  "Exafunction/codeium.nvim",
-  commit = "937667b2cadc7905e6b9ba18ecf84694cf227567", -- just guessing the commit sha here
-  -- ... rest of your config
+	-- codeium cmp source
+	{
+		"nvim-cmp",
+		dependencies = {
+			-- codeium
+			{
+				"Exafunction/codeium.nvim",
+				cmd = "Codeium",
+				build = ":Codeium Auth",
+				opts = {},
+			},
+		},
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			table.insert(opts.sources, 1, {
+				name = "codeium",
+				group_index = 1,
+				priority = 100,
+			})
+		end,
+	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		optional = true,
+		event = "VeryLazy",
+		opts = function(_, opts)
+			table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("codeium"))
+		end,
+	},
 }
