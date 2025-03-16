e8693d2ea17935e124dcfa3ceae47e1f30ba4088
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-03-16 18:50:46 -0300

    feat: added ngw-look pre-installation check

--- .dotfiles-install/modules/ngwlook.sh
+++ .dotfiles-install/modules/ngwlook.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if which 'nwg-look' >/dev/null; then
+  echo "ngw-look already installed"
+  exit 1
+fi
+
 git clone https://github.com/nwg-piotr/nwg-look.git
 cd nwg-look || exit
 make build
