b316744d7f4753a017d6ba5ac92716bd7d3c06a4
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-03-16 18:50:46 -0300

    feat(nvim): add latex snips, improve latex snips

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,60 +1,44 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    lazy = true,
-    build = "make install_jsregexp",
-    config = function(_, opts)
-      require("luasnip.loaders.from_lua").load({ paths = "./luasnip/" })
-      require("luasnip").config.set_config(opts)
-    end,
-    dependencies = {
-      {
-        "rafamadriz/friendly-snippets",
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-      },
-      {
-        "nvim-cmp",
-        dependencies = {
-          "saadparwaiz1/cmp_luasnip",
-        },
-        opts = function(_, opts)
-          opts.snippet = {
-            expand = function(args)
-              require("luasnip").lsp_expand(args.body)
-            end,
-          }
-          table.insert(opts.sources, { name = "luasnip" })
-        end,
-      },
-    },
-    opts = {
-      history = true,
-      store_selection_keys = "<Tab>",
-      enable_autosnippets = true,
-      update_events = { "TextChanged", "TextChangedI" },
-      region_check_events = "CursorHold,InsertLeave",
-      delete_check_events = "TextChanged,InsertEnter",
-    },
-  },
-  {
-    "nvim-cmp",
-    -- stylua: ignore
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true, silent = true, mode = "i",
-      },
-      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-    },
-  },
-  {
-    "garymjr/nvim-snippets",
-    enabled = false,
-  },
+	{
+		"L3MON4D3/LuaSnip",
+		lazy = true,
+		build = "make install_jsregexp",
+		config = function(_, opts)
+			require("luasnip.loaders.from_lua").load({ paths = "./luasnip/" })
+			require("luasnip").config.set_config(opts)
+		end,
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			{
+				"nvim-cmp",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+				},
+				opts = function(_, opts)
+					opts.snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					}
+					table.insert(opts.sources, { name = "luasnip" })
+				end,
+			},
+		},
+		opts = {
+			history = true,
+			store_selection_keys = "<Tab>",
+			enable_autosnippets = true,
+			update_events = "TextChanged,TextChangedI",
+			delete_check_events = "TextChanged",
+		},
+	},
+	{
+		"garymjr/nvim-snippets",
+		enabled = false,
+	},
 }
