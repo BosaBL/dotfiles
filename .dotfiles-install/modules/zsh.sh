70e1057f1aa77cc37db061042eae85503d5f988a
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-03-16 18:50:46 -0300

    feat: added checks for ./modules/zsh.sh
    Added installation checks to avoid unnecessary downloads, installations
    or shell changes on the ./modules/zsh.sh script.

--- .dotfiles-install/modules/zsh.sh
+++ .dotfiles-install/modules/zsh.sh
@@ -3,23 +3,36 @@
 sudo dnf -y install "zsh"
 
 # oh-my-zsh
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" "--unattended"
+if [ ! -d "$HOME"/.oh-my-zsh/ ]; then
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" "--unattended"
+fi
 # p10k
-echo "Installing powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-# fzf-tab
-echo "Installing fzf-tab"
-git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab"
-# zsh-autosuggestions
-echo "Installing zsh-autosuggestions"g
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-# zsh-syntax-highlighting
-echo "Installing zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-# zsh-vi-mode
-echo "Installing zsh-vi-mode"
-git clone https://github.com/jeffreytse/zsh-vi-mode "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode"
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k ]; then
+  echo "Installing powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+fi
 
-echo "Changing default shell to zsh"
-chsh -s "$(which zsh)"
+# fzf-tab
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/fzf-tab ]; then
+  echo "Installing fzf-tab"
+  git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab"
+fi
+
+# zsh-autosuggestions
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions ]; then
+  echo "Installing zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+fi
+
+# zsh-syntax-highlighting
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting ]; then
+  echo "Installing zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+fi
+
+# if current user's shell isn't zsh, use it
+if [ -n "$(rg -o 'chris:/usr/bin/zsh' <'/etc/passwd')" ]; then
+  echo "Changing default shell to zsh"
+  chsh -s "$(which zsh)"
+fi
