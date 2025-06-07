401472851a35e62efb51794fbd9ca29ea2a976e8
Christopher <43054246+BosaBL@users.noreply.github.com>
2025-06-07 18:57:39 -0400

    feat(theming): change themes to kanagawa

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,16 +1,42 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
-	{
-		"catppuccin/nvim",
-		enabled = false,
-	},
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+  {
+    "thesimonho/kanagawa-paper.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      colors = {
+        theme = {
+          ink = {
+            ui = {
+              float = {
+                bg = "none",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-paper-ink",
+    },
+  },
 }
