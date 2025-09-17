8b7b7be58717b350f3d4d82981e45d13ea03e30f
Christopher S. <43054246+BosaBL@users.noreply.github.com>
2025-09-17 16:43:10 -0300

    feat(nvim): update lazyvim to 15.x

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -48,7 +48,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
 
     { import = "plugins" },
-    { import = "plugins.lsp" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
