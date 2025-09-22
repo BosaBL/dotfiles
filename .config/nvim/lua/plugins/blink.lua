2dcb52b39db1d420fc84023ef802ea6a8cdfd639
Christopher S. <43054246+BosaBL@users.noreply.github.com>
2025-09-22 02:19:09 -0300

    chore(nvim): updates

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
+
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+    },
+    signature = { enabled = true },
+  },
+}
---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      -- Only needed one time to setup copilot for codecompanion.
+      -- "github/copilot.nvim",
+    },
+    opts = {},
+  },
+}
